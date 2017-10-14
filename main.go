90d609c38ef674b3800d8df142299c9134d6d24c
Shannon Beach <shannonbeach@Shannons-MBP-2.attlocal.net>
2017-10-14 16:41:04 -0400

    added function to get card

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ func main() {
 	//var card string = "Ace of Spades"
 
 	//only use := when assigning a new variable
-	card := "Ace of Spades"
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+
+	return "Five of Diamonds"
 }
